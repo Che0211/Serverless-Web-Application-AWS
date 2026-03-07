b2461f7b3b48b77e8de627bd34f0190428aaf7d5
chetan devadiga <165365121+Che0211@users.noreply.github.com>
2026-03-07 20:04:41 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,15 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_6YKezcsmk
+        userPoolId: 'us-east-1_PlmnMCvvo
 ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6jrfos8103j6jsr7v01lq1a4dg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3qghhfqdescu03l7mlcbe1jljl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
